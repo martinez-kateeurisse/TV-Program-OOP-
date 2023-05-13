740caf97dfc8324cb47cf5297bc727dcec315bd7
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-13 18:43:29 +0800

    Test Driver program instructions (pseudocode)

---
+++ TestTV.py
@@ -0,0 +1,15 @@
+#Kate Eurisse L. Martinez_BSCPE 1-5_Test Driver program
+
+#The current channel (1 to 120) of this TV.
+#The current volume level (1 to 7) of this TV.
+#Indicates whether this TV is on/off.
+
+#Constructs a default TV object.
+#Turns on this TV.
+#Turns off this TV.
+#Returns the channe; for this TV.
+#Sets a new volume level for this TV.
+#Increases the channel number by 1.
+#Decreases the channel number by 1.
+#Increases the volume level by 1.
+#Decreases the volume level by 1.
