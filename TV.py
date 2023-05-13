d9da4f609119a9f223d712113287b545282b9c6a
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-13 18:52:03 +0800

    Class TV - instructions

---
+++ TV.py
@@ -0,0 +1,17 @@
+#Kate Eurisse L. Martinez_BSCPE 1-5_TV-Class
+
+#channel: int
+#volume_level: int
+#on: boolean
+
+#TV ()
+#turnOn(): None
+#turnOff(): None
+#getChannel(): int
+#setChannel (channel: int): None
+#getVolume(): int
+#setVolume (volumeLevel: int): None
+#channelUp(): None
+#channelDown(): None
+#volumeUp(): None
+#volumeDown(): None
