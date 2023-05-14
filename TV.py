a643bd5ecc29bafc1d8de44f10f3019f66471d81
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-15 00:02:21 +0800

    Transfered the code to the class file

--- TV.py
+++ TV.py
@@ -1,5 +1,25 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_TV-Class
 
+class TV:
+    #Create Objects
+    def __init__ (self, channel, volume_level, on):
+        self.channel = channel
+        self.volume_level = volume_level
+        self.on = on
+
+    #Create Methods
+    def tv1(self):
+        return "tv1's channel is" + self.channel , "and volume level is" + self.volume_level 
+    def tv2(self):
+        return "tv2's channel is" + self.channel , "and volume level is" + self.volume_level 
+    
+#Assign values
+tv1 = TV(30, 3, "on")
+tv2 = TV(3, 2, "on")
+#Call methods
+print(tv1)
+print(tv2)
+
 #channel: int
 #volume_level: int
 #on: boolean
