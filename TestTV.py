e6ea2aeb420111613dbc5963f8e530df5dc0e340
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-15 10:15:59 +0800

    Testing file code flow - updated

--- TestTV.py
+++ TestTV.py
@@ -1,17 +1,7 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_Test Driver program
 
-#channel: int
-#volume_level: int
-#on: boolean
+#Import class file
 
-#TV ()
-#turnOn(): None
-#turnOff(): None
-#getChannel(): int
-#setChannel (channel: int): None
-#getVolume(): int
-#setVolume (volumeLevel: int): None
-#channelUp(): None
-#channelDown(): None
-#volumeUp(): None
-#volumeDown(): None
+#Assign values
+#Call methods
+#Print output
