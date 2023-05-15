9e01e7666f2ffa89b9ca494b6dc0c8d256fc145f
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-15 15:21:03 +0800

    Tryin' to figure out the import error - brb

--- TestTV.py
+++ TestTV.py
@@ -1,10 +1,14 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_Test Driver program
 
 #Import class file
-import TV
+#from TV import television
+from TV import television
+TV = television()
 
 #Assign values
-tv1 = TV(30, 3, "on")
-tv1 = TV(3, 2, "on")
+tv1 = television(30, 3, "on")
+tv1 = television(3, 2, "on")
 #Call methods
+tv1 = television()
+print(tv1)
 #Print output
