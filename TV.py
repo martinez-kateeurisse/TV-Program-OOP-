2faeb6e10ff6a84da307f221a8fd760beadebaa4
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-15 08:27:13 +0800

    Previously uploaded file instructions - exchanged

--- TV.py
+++ TV.py
@@ -1,37 +1,15 @@
 #Kate Eurisse L. Martinez_BSCPE 1-5_TV-Class
 
-class TV:
-    #Create Objects
-    def __init__ (self, channel, volume_level, on):
-        self.channel = channel
-        self.volume_level = volume_level
-        self.on = on
+#The current channel (1 to 120) of this TV.
+#The current volume level (1 to 7) of this TV.
+#Indicates whether this TV is on/off.
 
-    #Create Methods
-    def tv1(self):
-        print("tv1's channel is" + self.channel , "and volume level is" + self.volume_level)
-    def tv2(self):
-        print("tv2's channel is" + self.channel , "and volume level is" + self.volume_level)
-    
-#Assign values
-tv1 = TV(30, 3, "on")
-tv2 = TV(3, 2, "on")
-#Call methods
-tv1.tv1()
-tv1.tv1()
-
-#channel: int
-#volume_level: int
-#on: boolean
-
-#TV ()
-#turnOn(): None
-#turnOff(): None
-#getChannel(): int
-#setChannel (channel: int): None
-#getVolume(): int
-#setVolume (volumeLevel: int): None
-#channelUp(): None
-#channelDown(): None
-#volumeUp(): None
-#volumeDown(): None
+#Constructs a default TV object.
+#Turns on this TV.
+#Turns off this TV.
+#Returns the channe; for this TV.
+#Sets a new volume level for this TV.
+#Increases the channel number by 1.
+#Decreases the channel number by 1.
+#Increases the volume level by 1.
+#Decreases the volume level by 1.
