db75937abe2fac1a94de5e2444eb686a2b96040e
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-05-16 19:34:09 +0800

    Yeyyy, finally done (Program - fixed values version)

--- TestTV.py
+++ TestTV.py
@@ -3,7 +3,7 @@
 #Import class file
 from Television import TV
 
-#call methods for tv1
+#Call methods for tv1
 tv1 = TV()
 #Turning tv1 on
 tv1.tv_on()
@@ -11,9 +11,6 @@ tv1.tv_on()
 tv1.set_channel(30)
 #Settng volume for tv1
 tv1.set_volume(3)
-
-#Print output for tv 1
-#tv1 = print("tv1's channel is " + tv1.get_channel(), "and volume level is " +  tv1.get_volume())
 
 #Call methods for tv2
 tv2 = TV()
@@ -24,10 +21,8 @@ tv2.set_channel(3)
 #Setting volume for tv2
 tv2.set_volume(2)
 
-#Print output for tv 2
-#tv2 = print ("tv2's channel is " + tv2.get_channel(), "and volume level is " +  tv2.get_volume())
-
 #Displaying output through pygame
+
 #Import modules 
 import pygame
 from pygame.locals import *
@@ -54,8 +49,9 @@ while True:
     output_font = pygame.font.Font("awesome.ttf", 30)
     text_rect_tv1 = output_font.render("", True, (250,250,250)).get_rect()
     text_rect_tv2 = output_font.render("", True, (250,250,250)).get_rect()       
-    
-    #Render text surfaces with the current values
+    background = pygame.image.load("background.jpg")  
+      
+    #Render text surfaces with the current values (text outputs)
     output_tv1 = output_font.render("tv1's channel is " + str(tv1.get_channel()) + " and volume level is " +  str(tv1.get_volume()), True, (250,250,250))
     output_tv2 = output_font.render("tv2's channel is " + str(tv2.get_channel()) + " and volume level is " +  str(tv2.get_volume()), True, (250,250,250))
     
@@ -68,9 +64,8 @@ while True:
     text_rect_tv1.y -= output_tv1.get_height() // 2
     text_rect_tv2.x -= output_tv2.get_width() // 2
     text_rect_tv2.y -= output_tv2.get_height() // 2
-    background = pygame.image.load("background.jpg")
    
-    #Update display with the latest text surfaces and background
+    #Update display 
     display.fill((0,0,0))
     display.blit(background, (0,0))
     display.blit(output_tv1, text_rect_tv1)
